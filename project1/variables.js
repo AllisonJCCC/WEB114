d6f7a19f799b604952dc51adb28be363efee0e2b
Ahanna <ahanna1@jccc.edu>
2025-10-06 06:48:25 -0500

    Add files via upload

---
+++ project1/variables.js
@@ -0,0 +1,31 @@
+//Allison Hanna 10/5/2025 Project 1 variables
+
+//Step 2
+let myName = "Allison";
+let favoriteColor = "purple";
+let hobby = "bicycling";
+let favoriteFood = "pasta";
+let hasPet=true;
+
+//Step 3
+console.log("My name is", myName);
+console.log("My favorite color is" + favoriteColor);
+console.log(`My favorite hobby is ${hobby}`);
+console.log("And my favorite food is", favoriteFood);
+
+//Step 4
+console.log(`Hello! My name is ${myName}.`);
+console.log(`My favorite color is ${favoriteColor}.`);
+console.log(`One of my hobbies is ${hobby}.`);
+console.log(`I could eat ${favoriteFood} every day!`);
+
+//Part 2
+let x = 5; 
+x *= 2; // x was multiplied by 2, so x becomes 10
+let y = 1 + x; // y adds 1 to x’s new value (10), so y becomes 11
+
+//Part 3
+
+console.log("It is " + hasPet + " that I have a pet.");
+console.log(`It is ${hasPet} that I have a pet`);
+
